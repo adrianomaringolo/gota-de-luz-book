362a5686038788de4df13f3c640342e502af4125
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2022-03-24 12:00:04 -0300

    chore: new page

---
+++ pages/divulgacao/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/Layout";
+
+const Home = () => (
+  <>
+    <Layout title="Gota de Luz">
+      <h1>Divulgação - página em construção</h1>
+    </Layout>
+  </>
+);
+
+export default Home;
