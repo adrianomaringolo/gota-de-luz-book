746275396ea7bc98f5f2a5f70200908061733d7b
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2022-04-12 17:38:50 -0300

    chore: remove images from code

--- src/components/main-areas/GalleryArea.tsx
+++ src/components/main-areas/GalleryArea.tsx
@@ -7,7 +7,7 @@ export const GalleryArea: React.FC = () => {
       id="instagram"
       className={cn("section", styles.section, styles.sectionPhotos)}
     >
-      <div data-mc-src="84ee78f0-5e3f-471a-8642-6d8a3902f6a1#instagram"></div>
+      <div data-mc-src="081fc4b5-3220-4eb0-ab40-df71af557538#instagram"></div>
 
       <script
         src="https://cdn2.woxo.tech/a.js#62559b52c89f71002fada248"
