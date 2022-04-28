1d1034fdb724350a0fd68f3ea0529b8b58049d81
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2022-04-28 09:49:19 -0300

    chore: improve layout of product

--- src/components/ProductItemDisplay.tsx
+++ src/components/ProductItemDisplay.tsx
@@ -1,8 +1,164 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 import { ProductItem } from "../interfaces";
 import { CartService } from "../services/CartService";
 import { formatCurrency } from "../utils/format";
-import styles from "./../styles/products.module.scss";
+
+const StyledProductItemDisplay = styled.div`
+  .itemContainer {
+    @media only screen and (min-width: 768px) {
+      min-width: 300px;
+    }
+
+    position: relative;
+    overflow: hidden;
+    flex: 1;
+    min-width: 200px;
+    max-width: 500px;
+    height: 300px;
+    margin: 3px;
+    border: 1px solid #ccc;
+
+    &:hover {
+      .productText {
+        opacity: 1;
+      }
+    }
+  }
+
+  .productText {
+    position: absolute;
+    //opacity: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background: rgba(255, 255, 255, 0.8);
+    transition: all 0.5s;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    padding: 10px;
+  }
+
+  .moreButton {
+    font-size: 1em;
+    border: 1px solid #444;
+    color: #444;
+    background-color: transparent;
+    padding: 10px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.5s;
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.4);
+    }
+  }
+
+  .item {
+    height: 100%;
+  }
+
+  .itemTitle {
+    font-weight: 500;
+    font-size: 1.3rem;
+    border-width: 1px 0;
+    border-style: solid;
+    border-color: #000;
+    line-height: 1.2rem;
+    padding: 10px 0;
+  }
+
+  .itemDesc {
+    font-size: 1rem;
+    line-height: 1.1rem;
+  }
+
+  .modal {
+    visibility: hidden;
+    opacity: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    transition: all 0.5s;
+    overflow: auto;
+    position: absolute;
+    left: 0;
+    .modalItem {
+      display: none;
+      position: absolute;
+      top: 0;
+    }
+    &.expanded {
+      opacity: 1;
+      visibility: visible;
+      display: flex;
+      flex-wrap: wrap;
+      position: fixed;
+      z-index: 10;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      .modalItem {
+        display: block;
+      }
+    }
+  }
+  .detailedItem {
+    background-color: #fff;
+    max-width: 1000px;
+    width: 100%;
+    padding: 25px;
+
+    @media only screen and (min-width: 768px) {
+      padding: 50px;
+      .infoOverflowArea {
+        max-height: 80vh;
+      }
+    }
+    .infoOverflowArea {
+      display: flex;
+      flex-wrap: wrap;
+      overflow-x: auto;
+      max-height: 400px;
+    }
+    .itemTitle {
+      margin-bottom: 50px;
+    }
+    .image {
+      min-width: 300px;
+      min-height: 300px;
+      flex: 1;
+      margin-top: 25px;
+    }
+    .detailedDescription {
+      flex: 1;
+      margin-top: 25px;
+      text-align: left;
+      min-width: 300px;
+      @media only screen and (min-width: 768px) {
+        margin-left: 25px;
+      }
+    }
+    .buttonArea {
+      text-align: right;
+      margin-top: 20px;
+    }
+    button {
+      padding: 15px;
+      border: 1px solid #333;
+      background-color: transparent;
+      cursor: pointer;
+      border-radius: 5px;
+      font-size: 1rem;
+      transition: background-color 0.5s;
+      &:hover {
+        background-color: rgba(0, 0, 0, 0.1);
+      }
+    }
+  }
+`;
 
 const ProductItemDisplay = ({
   item,
@@ -23,16 +179,16 @@ const ProductItemDisplay = ({
   };
 
   return (
-    <>
-      <div className={styles.itemContainer} key={item.name}>
+    <StyledProductItemDisplay>
+      <div className="itemContainer" key={item.name}>
         <div
-          className={styles.item}
+          className="item"
           style={{
             background: `url('${item.image}') no-repeat center / cover`,
           }}
         ></div>
-        <div className={styles.productText}>
-          <p className={styles.itemTitle}>{item.name}</p>
+        <div className="productText">
+          <p className="itemTitle">{item.name}</p>
           {!item.available ? (
             <p>
               <big>
@@ -40,17 +196,17 @@ const ProductItemDisplay = ({
               </big>
             </p>
           ) : (
-            <p>
+            <p style={{ marginBottom: 0 }}>
               <strong>{formatCurrency(item.price)}</strong>
             </p>
           )}
-          <p className={styles.itemDesc}>{item.description}</p>
+          <p className="itemDesc">{item.description}</p>
           <div style={{ display: "flex" }}>
             {item.detailedDescription && (
               <button
                 style={{ margin: "0 5px" }}
-                onClick={() => setViewMode(styles.expanded)}
-                className={styles.moreButton}
+                onClick={() => setViewMode("expanded")}
+                className="moreButton"
               >
                 Saiba mais
               </button>
@@ -59,7 +215,7 @@ const ProductItemDisplay = ({
               <button
                 style={{ margin: "0 5px", display: "flex" }}
                 onClick={() => addToCart()}
-                className={styles.moreButton}
+                className="moreButton"
               >
                 <img
                   src="/images/shopping-bag.png"
@@ -71,32 +227,32 @@ const ProductItemDisplay = ({
           </div>
         </div>
       </div>
-      <div className={`${styles.modal} ${viewMode}`}>
-        <div className={`${styles.detailedItem} ${styles.modalItem}`}>
-          <p className={styles.itemTitle}>
+      <div className={`modal ${viewMode}`}>
+        <div className={`detailedItem modalItem`}>
+          <p className="itemTitle">
             {type}: {item.name}
           </p>
-          <div className={styles.infoOverflowArea}>
+          <div className="infoOverflowArea">
             <div
-              className={styles.image}
+              className="image"
               style={{
                 background: `url('${item.image}') no-repeat center / contain`,
                 backgroundPosition: "top",
               }}
             ></div>
             <div
-              className={styles.detailedDescription}
+              className="detailedDescription"
               dangerouslySetInnerHTML={{
                 __html: item.detailedDescription || "",
               }}
             ></div>
           </div>
-          <div className={styles.buttonArea}>
+          <div className="buttonArea">
             <button onClick={() => setViewMode("")}>Entendi, obrigado!</button>
           </div>
         </div>
       </div>
-    </>
+    </StyledProductItemDisplay>
   );
 };
 
