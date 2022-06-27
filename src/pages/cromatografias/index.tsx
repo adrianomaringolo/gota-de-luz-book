654de466a6824b4ed948d1372fb2493c8789eebf
Adriano Maringolo <adrianomaringolo@gmail.com>
2022-06-26 21:02:21 -0300

    docs: adding github information

--- src/pages/cromatografias/index.tsx
+++ src/pages/cromatografias/index.tsx
@@ -30,13 +30,43 @@ const Cromatografias = () => (
         </p>
         <ul>
           <li>
-            <a href="/pdf/cromatografia-lavanda.pdf" target="_blank">
-              Lavanda (<i>Lavandula dentata</i>)
+            <a
+              href="https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Lavanda%20dentata%20e%20Pelargonium%20graveolens.pdf?alt=media&token=9289ec29-2ab2-4995-abc9-8d0270ec01cc"
+              target="_blank"
+            >
+              Lavanda (<i>Lavandula dentata</i> e <i>Cymbopogon citratus</i>)
             </a>
           </li>
           <li>
-            <a href="/pdf/cromatografia-melaleuca.pdf" target="_blank">
+            <a
+              href="https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Melaleuca%20alternifolia.pdf?alt=media&token=ecca7ff8-4534-49a0-a278-c20a771fa237"
+              target="_blank"
+            >
               Melaleuca (<i>Melaleuca alternifolia</i>)
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Melaleuca%20alternifolia.pdf?alt=media&token=ecca7ff8-4534-49a0-a278-c20a771fa237"
+              target="_blank"
+            >
+              Melaleuca (<i>Melaleuca alternifolia</i>)
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Cymbopogon%20citratus.pdf?alt=media&token=66341630-2a54-4d6a-87af-b7a92ac958a8"
+              target="_blank"
+            >
+              Capim limão (<i>Cymbopogon citratus</i>)
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Pelargonium%20graveolens.pdf?alt=media&token=67365247-8a6b-40fe-a562-059a1d3eebc7"
+              target="_blank"
+            >
+              Gerânio (<i>Pelargonium graveolens</i>)
             </a>
           </li>
         </ul>
