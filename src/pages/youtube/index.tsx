31f58abc1384ce25b328ec66688c8cb03a86e68d
Adriano Maringolo <adrianomaringolo@gmail.com>
2022-07-29 16:56:52 -0300

    chore: new route to youtube

---
+++ src/pages/youtube/index.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+const Youtube = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://www.youtube.com/channel/UCq6y4YcG2WBqZyM5Y5MhNoA";
+  }, []);
+
+  return <></>;
+};
+
+export default Youtube;
