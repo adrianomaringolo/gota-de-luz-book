edb5add26777af26f0043673e05f9b0696ccfb94
Adriano Maringolo <adrianomaringolo@gmail.com>
2022-08-23 15:27:42 -0300

    chore: add testimony link

---
+++ src/pages/depoimento/index.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+const Depoimento = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://docs.google.com/forms/d/e/1FAIpQLSetBUhLfPUyn-AAaeZFSluLuB3BEzrpEX0yirA2CPk6LklYWg/viewform";
+  }, []);
+
+  return <></>;
+};
+
+export default Depoimento;
