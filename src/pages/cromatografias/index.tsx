7da970f380be21c0173bfc79f4a1318847508c2b
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2024-03-20 15:14:55 -0300

    add new reports

--- src/pages/cromatografias/index.tsx
+++ src/pages/cromatografias/index.tsx
@@ -1,80 +1,90 @@
-import styled from "styled-components";
-import Layout from "../../components/Layout";
+import styled from 'styled-components'
+import Layout from '../../components/Layout'
 
 const StyledArea = styled.section`
   padding: 80px 10px;
   max-width: 600px;
   margin: 0 auto;
-`;
+`
 
 const reports = [
   {
-    name: "Lavanda e Capim limão - Óleo essencial",
-    cientificName: "Lavandula dentata e Cymbopogon citratus",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Lavanda%20dentata%20e%20Pelargonium%20graveolens.pdf?alt=media&token=9289ec29-2ab2-4995-abc9-8d0270ec01cc",
+    name: 'Lavanda e Capim limão - Óleo essencial',
+    cientificName: 'Lavandula dentata e Cymbopogon citratus',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Lavanda%20dentata%20e%20Pelargonium%20graveolens.pdf?alt=media&token=9289ec29-2ab2-4995-abc9-8d0270ec01cc',
   },
   {
-    name: "Melaleuca - Óleo essencial",
-    cientificName: "Melaleuca alternifolia",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Melaleuca%20alternifolia.pdf?alt=media&token=ecca7ff8-4534-49a0-a278-c20a771fa237",
+    name: 'Melaleuca - Óleo essencial',
+    cientificName: 'Melaleuca alternifolia',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Melaleuca%20alternifolia.pdf?alt=media&token=ecca7ff8-4534-49a0-a278-c20a771fa237',
   },
   {
-    name: "Capim limão - Óleo essencial",
-    cientificName: "Cymbopogon citratus",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Cymbopogon%20citratus.pdf?alt=media&token=66341630-2a54-4d6a-87af-b7a92ac958a8",
+    name: 'Capim limão - Óleo essencial',
+    cientificName: 'Cymbopogon citratus',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Cymbopogon%20citratus.pdf?alt=media&token=66341630-2a54-4d6a-87af-b7a92ac958a8',
   },
   {
-    name: "Gerânio - Óleo essencial",
-    cientificName: "Pelargonium graveolens",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Pelargonium%20graveolens.pdf?alt=media&token=67365247-8a6b-40fe-a562-059a1d3eebc7",
+    name: 'Gerânio - Óleo essencial',
+    cientificName: 'Pelargonium graveolens',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Pelargonium%20graveolens.pdf?alt=media&token=67365247-8a6b-40fe-a562-059a1d3eebc7',
   },
   {
-    name: "Jambolão - Óleo essencial",
-    cientificName: "Syzygium cumini",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Syzygium%20cumini.pdf?alt=media&token=6e80af9a-4f80-48af-86a2-e3dbc55f84e4",
+    name: 'Jambolão - Óleo essencial',
+    cientificName: 'Syzygium cumini',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Syzygium%20cumini.pdf?alt=media&token=6e80af9a-4f80-48af-86a2-e3dbc55f84e4',
   },
   {
-    name: "Jambolão - Hidrolato",
-    cientificName: "Syzygium cumini",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Syzygium%20cumini%20-%20Hidrolato.pdf?alt=media&token=9b2b34d7-a1ae-4e98-971c-34972c0886bf",
+    name: 'Jambolão - Hidrolato',
+    cientificName: 'Syzygium cumini',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Syzygium%20cumini%20-%20Hidrolato.pdf?alt=media&token=9b2b34d7-a1ae-4e98-971c-34972c0886bf',
   },
   {
-    name: "Guaçatonga - Hidrolato",
-    cientificName: "Casearia sylvestris",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Casearia%20sylvestris.pdf?alt=media&token=c9da6805-20b1-455b-94c4-e8f21a5536f5",
+    name: 'Guaçatonga - Hidrolato',
+    cientificName: 'Casearia sylvestris',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelat%C3%B3rio%20T%C3%A9cnico%20-%20Casearia%20sylvestris.pdf?alt=media&token=c9da6805-20b1-455b-94c4-e8f21a5536f5',
   },
   {
-    name: "Manjericão - Óleo essencial",
-    cientificName: "Ocimum basilicum",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Ocimum%20basilicum.pdf?alt=media&token=15c58e8b-cb45-4a8d-ae48-f8c7970d4aef",
+    name: 'Manjericão - Óleo essencial',
+    cientificName: 'Ocimum basilicum',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Ocimum%20basilicum.pdf?alt=media&token=15c58e8b-cb45-4a8d-ae48-f8c7970d4aef',
   },
 
   {
-    name: "Palmarosa - Óleo essencial",
-    cientificName: "Cymbopogon martini",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Cymbopogon%20martini.pdf?alt=media&token=25f43f47-2a2b-4445-9c2a-94f31e32f6fe",
+    name: 'Palmarosa - Óleo essencial',
+    cientificName: 'Cymbopogon martini',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Cymbopogon%20martini.pdf?alt=media&token=25f43f47-2a2b-4445-9c2a-94f31e32f6fe',
   },
   {
-    name: "Immortelle - Óleo essencial",
-    cientificName: "Helichrysum italicum",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Helichrysum%20italicum.pdf?alt=media&token=80f8a211-435d-4b28-99c0-39201535bbd5",
+    name: 'Immortelle - Óleo essencial',
+    cientificName: 'Helichrysum italicum',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Helichrysum%20italicum.pdf?alt=media&token=80f8a211-435d-4b28-99c0-39201535bbd5',
   },
   {
-    name: "Louro - Óleo essencial",
-    cientificName: "Laurus nobilis",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Laurus%20nobilis.pdf?alt=media&token=f550adaa-feae-4ed2-806f-86b8ed99eb2c",
+    name: 'Louro - Óleo essencial',
+    cientificName: 'Laurus nobilis',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Laurus%20nobilis.pdf?alt=media&token=f550adaa-feae-4ed2-806f-86b8ed99eb2c',
   },
   {
-    name: "Cipreste - Óleo essencial",
-    cientificName: "Cupressus sempervirens",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Cupressus%20sempervirens.pdf?alt=media&token=2cd405d2-bb39-47f5-8778-a131a61f455f",
+    name: 'Cipreste - Óleo essencial',
+    cientificName: 'Cupressus sempervirens',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Cupressus%20sempervirens.pdf?alt=media&token=2cd405d2-bb39-47f5-8778-a131a61f455f',
   },
   {
-    name: "Malaleuca Branca - Óleo essencial",
-    cientificName: "Melaleuca armillaris",
-    url: "https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Melaleuca%20armillaris.pdf?alt=media&token=078c66c3-0e22-4e58-98f5-e38b4e3adde2",
+    name: 'Malaleuca Branca - Óleo essencial',
+    cientificName: 'Melaleuca armillaris',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Melaleuca%20armillaris.pdf?alt=media&token=078c66c3-0e22-4e58-98f5-e38b4e3adde2',
   },
-];
+  {
+    name: 'Erva Baleeira - Óleo essencial',
+    cientificName: 'Cordia verbenacea',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Cordia%20verbenacea%20-%20OE.pdf?alt=media&token=95fca0b6-9d28-499a-bf14-0170419c2431',
+  },
+  {
+    name: 'Erva Baleeira - Hidrolato',
+    cientificName: 'Cordia verbenacea',
+    url: 'https://firebasestorage.googleapis.com/v0/b/gota-de-luz.appspot.com/o/reports%2FRelato%CC%81rio%20Te%CC%81cnico%20-%20Cordia%20verbenacea%20-%20HID.pdf?alt=media&token=32ec37de-7844-4bb1-bdab-5841441c74ab',
+  },
+]
 
 const Cromatografias = () => (
   <>
@@ -82,35 +92,34 @@ const Cromatografias = () => (
       <StyledArea>
         <h1 className="text-2xl font-bold my-5">Cromatografias</h1>
 
-        <p>
-          Cromatografia é um processo de separação e identificação de
-          componentes de uma mistura.
+        <p className="mb-5">
+          Cromatografia é um processo de separação e identificação de componentes de uma
+          mistura.
         </p>
-        <p>
-          A cromatografia serve para identificação de substâncias, purificação
-          de compostos e separação de componentes de misturas.
+        <p className="mb-5">
+          A cromatografia serve para identificação de substâncias, purificação de
+          compostos e separação de componentes de misturas.
         </p>
 
-        <hr />
-        <p>
-          Confira abaixo as cromatografias realizadas a partir dos óleos
-          essenciais destilados com as plantas cultivadas na Chácara da Mãe
-          Luzia.
+        <p className="mb-5 pt-5 border-t">
+          Confira abaixo as cromatografias realizadas a partir dos óleos essenciais
+          destilados com as plantas cultivadas na Chácara da Mãe Luzia.
         </p>
-        <ul className="mt-5">
+        <div className="gap-3 grid sm:grid-cols-1 md:grid-cols-2 mt-5">
           {reports
             .sort((a, b) => a.name.localeCompare(b.name))
             .map(({ name, url, cientificName }) => (
-              <li className="ml-4 list-item">
-                <a href={url} target="_blank">
-                  {name} (<i>{cientificName}</i>)
+              <div key={`croma-${url}`} className="border rounded hover:bg-gray-200">
+                <a href={url} target="_blank" className="p-4 block text-center">
+                  {name}
+                  <br />(<i>{cientificName}</i>)
                 </a>
-              </li>
+              </div>
             ))}
-        </ul>
+        </div>
       </StyledArea>
     </Layout>
   </>
-);
+)
 
-export default Cromatografias;
+export default Cromatografias
