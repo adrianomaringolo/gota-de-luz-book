4607b6031cf061558f893c6f3e1dba312c73cb99
Adriano Maringolo Gomes <adrianomaringolo@gmail.com>
2024-03-29 23:54:25 -0300

    Refactor ConfirmationOrder component

--- src/components/Cart/ConfirmationOrder.tsx
+++ src/components/Cart/ConfirmationOrder.tsx
@@ -1,40 +1,38 @@
-import styled from "styled-components";
-import { CartItemType } from "../../services/CartService";
-import { formatCurrency } from "../../utils/format";
+import { InputField } from 'components/shared'
+import { useState } from 'react'
+import styled from 'styled-components'
+import { CartItemType } from '../../services/CartService'
+import { formatCurrency } from '../../utils/format'
 
-const StyledContactForm = styled.div``;
+const StyledContactForm = styled.div``
 
 export const ConfirmationOrder = ({
   cart,
   contactInfo,
 }: {
-  cart: any;
-  contactInfo: any;
+  cart: any
+  contactInfo: any
 }) => {
+  const [showCupom, setShowCupom] = useState(false)
   const getTotal = () => {
-    let total = 0;
+    let total = 0
     cart?.items.forEach((element: CartItemType) => {
-      total += element.price * (element.amount || 0);
-    });
+      total += element.price * (element.amount || 0)
+    })
 
-    return total;
-  };
+    return total
+  }
 
   return (
     <StyledContactForm>
       {cart?.items
         ?.filter((item: CartItemType) => item.amount)
         .map((item: CartItemType) => (
-          <div key={`${item.id}`} style={{ display: "flex" }}>
-            <div
-              className="item-price"
-              style={{ width: "80px", textAlign: "right" }}
-            >
+          <div key={`${item.id}`} style={{ display: 'flex' }}>
+            <div className="item-price" style={{ width: '80px', textAlign: 'right' }}>
               {formatCurrency(item.price * (item.amount || 0))}
             </div>
-            <div
-              style={{ width: "50px", textAlign: "right", marginRight: "10px" }}
-            >
+            <div style={{ width: '50px', textAlign: 'right', marginRight: '10px' }}>
               {item.amount} x
             </div>
             <span>
@@ -43,11 +41,23 @@ export const ConfirmationOrder = ({
           </div>
         ))}
 
-      <div className="total-value">
-        Valor total: {formatCurrency(getTotal())}
+      <div className="text-bold text-3xl mb-5 border-t pt-3 mt-3 flex justify-between items-center">
+        <div>
+          <span className="text-xl">Valor total:</span> {formatCurrency(getTotal())}
+        </div>
+        <p
+          className="text-blue-900 text-sm bg-none border-none cursor-pointer hover:text-black"
+          onClick={() => setShowCupom((prev) => !prev)}
+        >
+          Inserir cupom ou vale de desconto
+          {showCupom && (
+            <InputField placeholder="Cupom" className="border border-gray-300 p-1" />
+          )}
+        </p>
       </div>
 
-      <p>
+      <div className="border rounded-xl p-4 mb-5">
+        <p className="font-bold text-2xl">Meus dados</p>
         <b>Nome:</b> {contactInfo?.name}
         <br />
         <b>Telefone:</b> {contactInfo?.phone}
@@ -56,9 +66,9 @@ export const ConfirmationOrder = ({
         <br />
         <b>Cidade:</b> {contactInfo?.city} | <b>CEP:</b> {contactInfo?.zipcode}
         <br />
-        <b>Observações:</b> {contactInfo?.observations || "-"}
+        <b>Observações:</b> {contactInfo?.observations || '-'}
         <br />
-      </p>
+      </div>
     </StyledContactForm>
-  );
-};
+  )
+}
